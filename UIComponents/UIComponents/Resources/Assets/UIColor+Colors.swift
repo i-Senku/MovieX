f59489b87a0a275f5850eaf82dcdbd33a18ad602
i-Senku <ercngp@gmail.com>
2022-08-27 10:19:25 +0300

    add colors

--- UIComponents/UIComponents/Resources/Assets/UIColor+Colors.swift
+++ UIComponents/UIComponents/Resources/Assets/UIColor+Colors.swift
@@ -5,6 +5,26 @@ import UIKit
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public extension UIColor {
+
+  static var appDark: UIColor {
+      return Asset.Colors.appDark.color
+  }
+
+  static var appGreen: UIColor {
+      return Asset.Colors.appGreen.color
+  }
+
+  static var appSnow: UIColor {
+      return Asset.Colors.appSnow.color
+  }
+
+  static var appWhite: UIColor {
+      return Asset.Colors.appWhite.color
+  }
+
+  static var appYellow: UIColor {
+      return Asset.Colors.appYellow.color
+  }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 

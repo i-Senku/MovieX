55d3d17973ca3cc6b55c3ba8a3c5c2696f1a68d0
i-Senku <ercngp@gmail.com>
2022-08-27 10:40:59 +0300

    add constants

---
+++ DataProvider/DataProvider/Constants/RemoteConstants.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteConstants.swift
+//  DataProvider
+//
+//  Created by Ercan Garip on 27.08.2022.
+//
+
+import Foundation
+
+public struct RemoteConstants {
+    public static let apiKey = "daba6de4"
+}

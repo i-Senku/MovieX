566011d73fe65b23deb9a29460453af3e2fbbd73
i-Senku <ercngp@gmail.com>
2022-08-27 14:18:53 +0300

    change scene configuration to appdelegate

---
+++ MovieX/Configuration/AppDelegate/AppDelegate+Firebase.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate+Firebase.swift
+//  MovieX
+//
+//  Created by Ercan Garip on 27.08.2022.
+//
+
+import Foundation
+import Firebase
+
+extension AppDelegate {
+    
+    func configureFirebase() {
+        FirebaseApp.configure()
+    }
+}

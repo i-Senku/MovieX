26dce67af228b6d87ecdabe5bfd80c6e85682db5
i-Senku <ercngp@gmail.com>
2022-08-27 10:40:52 +0300

    add network core

---
+++ DataProvider/DataProvider/Network/HttpHeader.swift
@@ -0,0 +1,31 @@
+//
+//  HttpHeader.swift
+//  DataProvider
+//
+//  Created by Ercan Garip on 27.08.2022.
+//
+
+import Foundation
+
+public enum HttpHeader {
+    case contentType(value : String? = "application/json")
+    case bearerToken(token : String)
+    
+    var key : String {
+        switch self {
+        case .contentType:
+            return "Content-Type"
+        case .bearerToken(_):
+            return "Authorization"
+        }
+    }
+    
+    var value : String {
+        switch self {
+        case .contentType(let value):
+            return value ?? "application/json"
+        case .bearerToken(let token):
+            return "Bearer \(token)"
+        }
+    }
+}

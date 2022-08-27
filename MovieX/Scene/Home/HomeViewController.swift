d97cd975cf27f09f312d269e813da6c7f763fa54
i-Senku <ercngp@gmail.com>
2022-08-27 15:14:20 +0300

    add home scene with router

---
+++ MovieX/Scene/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  MovieX
+//
+//  Created by Ercan Garip on 27.08.2022.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController<HomeViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

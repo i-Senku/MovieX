b4167ee8e29062b038e0da63c42184facd5be68d
i-Senku <ercngp@gmail.com>
2022-08-27 15:14:11 +0300

    update splash scene

--- MovieX/Scene/Splash/SplashViewController.swift
+++ MovieX/Scene/Splash/SplashViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 import TinyConstraints
+import UIComponents
 
 final class SplashViewController: BaseViewController<SplashViewModel> {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .bold18
+        label.textColor = .appSnow
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubViews()
-        configureContents()
+        subscribeViewModel()
+        viewModel.viewDidLoad()
     }
 }
 
@@ -21,14 +30,36 @@ final class SplashViewController: BaseViewController<SplashViewModel> {
 extension SplashViewController {
     
     private func addSubViews() {
-        
+        addTitleLabel()
+    }
+    
+    private func addTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.centerInSuperview()
     }
 }
 
-// MARK: - ConfigureContents
+// MARK: - SubscribeViewModel
 extension SplashViewController {
     
-    private func configureContents() {
-        
+    private func subscribeViewModel() {
+        viewModel.updateTitle = { [weak self] text in
+            guard let self = self else { return }
+            self.titleLabel.text = text
+        }
+    }
+}
+
+// MARK: - RouteDelegate
+extension SplashViewController: SplashViewRouteDelegate {
+    
+    func showHome() {
+        guard let window = app.router.window else { return }
+        let homeViewController = HomeRouter.create()
+        UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            UIView.performWithoutAnimation {
+                window.rootViewController = homeViewController
+            }
+        }, completion: nil)
     }
 }
--- MovieX/Scene/Splash/SplashViewModel.swift
+++ MovieX/Scene/Splash/SplashViewModel.swift
@@ -6,13 +6,81 @@
 //
 
 import Foundation
+import DataProvider
+import FirebaseRemoteConfig
 
-protocol SplashViewDataSource {}
+protocol SplashViewDataSource {
+    var routeDelegate: SplashViewRouteDelegate? { get set }
+}
 
-protocol SplashViewEventSource {}
+protocol SplashViewEventSource {
+    var updateTitle: AnyClosure<String?>? { get set }
+}
 
-protocol SplashViewProtocol: SplashViewDataSource, SplashViewEventSource {}
+protocol SplashViewRouteDelegate: AnyObject {
+    func showHome()
+}
+
+protocol SplashViewProtocol: SplashViewDataSource, SplashViewEventSource {
+    func viewDidLoad()
+}
 
 final class SplashViewModel: BaseViewModel, SplashViewProtocol {
     
+    // Privates
+    private var remoteConfig: RemoteConfig?
+    
+    // EventSource
+    var updateTitle: AnyClosure<String?>?
+    
+    // DataSource
+    weak var routeDelegate: SplashViewRouteDelegate?
+    
+    func viewDidLoad() {
+        if Connectivity.hasInternetConnection {
+            configureRemoteConfig()
+        } else {
+            updateTitle?("İnternet bağlantısı bulunamadı")
+            EntryKitHelper.show("İnternet bağlantınızı kontrol ediniz.", type: .error)
+        }
+    }
+}
+
+// MARK: - Configure
+extension SplashViewModel {
+    
+    private func configureRemoteConfig() {
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig?.configSettings = settings
+        fetchRemoteConfig()
+    }
+}
+
+// MARK: - DataProvider
+extension SplashViewModel {
+    
+    private func fetchRemoteConfig() {
+        remoteConfig?.fetch { [weak self] (status, error) -> Void in
+            guard let self = self else { return }
+            if status == .success {
+                self.remoteConfig?.activate { changed, error in
+                    guard error == nil else {
+                        return
+                    }
+                    let title = self.remoteConfig?.configValue(forKey: "splash_title").stringValue
+                    DispatchQueue.main.async {
+                        self.updateTitle?(title)
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+                        guard let self = self else { return }
+                        self.routeDelegate?.showHome()
+                    }
+                }
+            } else {
+                EntryKitHelper.show(error?.localizedDescription, type: .success)
+            }
+        }
+    }
 }
